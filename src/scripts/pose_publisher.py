20db798861901b89317327de2750b5170dcf602d
Kickblip <wyatt.zilker@gmail.com>
2023-09-10 14:48:40 -0600

    randomize pose publishing

--- src/scripts/pose_publisher.py
+++ src/scripts/pose_publisher.py
@@ -2,11 +2,12 @@
 
 import rospy
 from geometry_msgs.msg import Pose
+import random
 
 def pose_publisher():
     # Initialize the node
     rospy.init_node('pose_publisher_node', anonymous=True)
-    
+
     # Create a Publisher object, specify the topic name, message type, and queue size
     pub = rospy.Publisher('/target_pose', Pose, queue_size=10)
 
@@ -16,17 +17,27 @@ def pose_publisher():
     while not rospy.is_shutdown():
         # Create a Pose message object
         target_pose = Pose()
-        
-        # Set the position (example values)
-        target_pose.position.x = 0.4
-        target_pose.position.y = 0.2
-        target_pose.position.z = 0.6
 
-        # Set the orientation (example values, in quaternion)
-        target_pose.orientation.x = 0.0
-        target_pose.orientation.y = 0.0
-        target_pose.orientation.z = 0.0
-        target_pose.orientation.w = 1.0
+        # Set random position within some reasonable bounds (modify as needed)
+        target_pose.position.x = random.uniform(0.1, 0.5)
+        target_pose.position.y = random.uniform(-0.2, 0.2)
+        target_pose.position.z = random.uniform(0.1, 0.5)
+
+        # Set random orientation (quaternion values; modify as needed)
+        target_pose.orientation.x = random.uniform(0, 1)
+        target_pose.orientation.y = random.uniform(0, 1)
+        target_pose.orientation.z = random.uniform(0, 1)
+        target_pose.orientation.w = random.uniform(0, 1)
+
+        # Normalize the quaternion (to make it a valid orientation)
+        norm_factor = (target_pose.orientation.x ** 2 + 
+                       target_pose.orientation.y ** 2 + 
+                       target_pose.orientation.z ** 2 + 
+                       target_pose.orientation.w ** 2) ** 0.5
+        target_pose.orientation.x /= norm_factor
+        target_pose.orientation.y /= norm_factor
+        target_pose.orientation.z /= norm_factor
+        target_pose.orientation.w /= norm_factor
 
         # Publish the Pose message
         pub.publish(target_pose)
