76537ca13029122491ec7d1de6d1611c82e034db
Kickblip <wyatt.zilker@gmail.com>
2023-10-04 08:32:57 -0600

    switch back to base_link

--- scripts/moving-arm/move_arm_camera_frame.py
+++ scripts/moving-arm/move_arm_camera_frame.py
@@ -6,17 +6,18 @@ from moveit_python import MoveGroupInterface, PlanningSceneInterface
 from fetch_test.srv import MoveToPose, MoveToPoseResponse
 import tf
 
+
 def handle_move_to_pose(req):
-    
+
     # Create a response object
     res = MoveToPoseResponse()
-    
+
     # Create move group interface for a fetch robot
     move_group = MoveGroupInterface("arm_with_torso", "head_camera_link")
-    
+
     # Initialize PlanningScene
     planning_scene = PlanningSceneInterface("head_camera_link")
-    
+
     # Initialize TF listener
     listener = tf.TransformListener()
 
@@ -28,31 +29,35 @@ def handle_move_to_pose(req):
         rospy.logerr("Received pose is not in 'base_link'")
         res.success = False
         return res
-    
+
     try:
         # Transform the pose from base_link to head_camera_link
-        pose_in_camera_link = listener.transformPose('head_camera_link', req.target_pose)
+        pose_in_camera_link = listener.transformPose(
+            'head_camera_link', req.target_pose)
     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        rospy.logerr("Failed to transform pose from base_link to head_camera_link")
+        rospy.logerr(
+            "Failed to transform pose from base_link to head_camera_link")
         res.success = False
         return res
-    
+
     # Move the robot arm to the target pose
     move_group.moveToPose(pose_in_camera_link, "wrist_roll_link")
     result = move_group.get_move_action().get_result()
-    
+
     if result:
         if result.error_code.val == MoveItErrorCodes.SUCCESS:
             rospy.loginfo("Moved to target pose!")
             res.success = True
         else:
-            rospy.logerr("Arm goal in state: %s", move_group.get_move_action().get_state())
+            rospy.logerr("Arm goal in state: %s",
+                         move_group.get_move_action().get_state())
             res.success = False
     else:
         rospy.logerr("MoveIt! failure no result returned.")
         res.success = False
 
     return res
+
 
 if __name__ == '__main__':
     rospy.init_node("move_to_target_pose_service")
