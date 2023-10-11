0ce8f597e7cf31e59d629c3eca46a0113492ebeb
Kickblip <wyatt.zilker@gmail.com>
2023-10-10 20:44:22 -0600

    dont need opencv stuff in the torso lift

--- scripts/pick-and-place-demo/head_and_back.py
+++ scripts/pick-and-place-demo/head_and_back.py
@@ -1,13 +1,8 @@
+#!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
 from moveit_msgs.msg import MoveItErrorCodes
 from moveit_python import MoveGroupInterface, PlanningSceneInterface
-import cv2
-
-
-bridge = CvBridge()
 
 
 rospy.init_node('torso_head_mover', anonymous=True)
@@ -15,19 +10,6 @@ rospy.init_node('torso_head_mover', anonymous=True)
 
 move_group = MoveGroupInterface("torso_and_head", "base_link")
 planning_scene = PlanningSceneInterface("base_link")
-
-
-def image_callback(img_msg):
-    global bridge
-    try:
-
-        cv_image = bridge.imgmsg_to_cv2(img_msg, "bgr8")
-    except CvBridgeError as e:
-        print(e)
-        return
-
-    cv2.imshow("Image Window", cv_image)
-    cv2.waitKey(3)
 
 
 def move_robot():
@@ -50,17 +32,8 @@ def move_robot():
     move_group.get_move_action().cancel_all_goals()
 
 
-image_topic = "/head_camera/rgb/image_raw"
-rospy.Subscriber(image_topic, Image, image_callback)
-
-
 rospy.sleep(1)
-
 
 move_robot()
 
-
 rospy.spin()
-
-
-cv2.destroyAllWindows()
