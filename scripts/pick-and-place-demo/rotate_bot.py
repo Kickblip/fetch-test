54bc0f28ab9f9c2d6e9edeacdb2e789670c572fe
Kickblip <wyatt.zilker@gmail.com>
2023-10-18 22:52:01 -0600

    move client in

--- scripts/pick-and-place-demo/rotate_bot.py
+++ scripts/pick-and-place-demo/rotate_bot.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python
 
+import copy
+import actionlib
 import rospy
-from geometry_msgs.msg import Twist
+
+from math import sin, cos
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+
+class MoveBaseClient(object):
+
+    def __init__(self):
+        self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        # rospy.loginfo("Waiting for move_base...")
+        # self.client.wait_for_server()
+
+    def goto(self, x, y, theta, frame="base_link"):
+        move_goal = MoveBaseGoal()
+        move_goal.target_pose.pose.position.x = x
+        move_goal.target_pose.pose.position.y = y
+        move_goal.target_pose.pose.orientation.z = sin(theta/2.0)
+        move_goal.target_pose.pose.orientation.w = cos(theta/2.0)
+        move_goal.target_pose.header.frame_id = frame
+        move_goal.target_pose.header.stamp = rospy.Time.now()
+
+        # TODO wait for things to work
+        self.client.send_goal(move_goal)
+        self.client.wait_for_result()
 
 
 def rotate_180_degrees():
-    # Initialize the ROS node
-    rospy.init_node('rotate_robot_180', anonymous=True)
-
-    # Publisher for cmd_vel
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-
-    rotation_duration = 1.0
-    rate = rospy.Rate(10)
-    end_time = rospy.Time.now() + rospy.Duration(rotation_duration)
-
-    print("Starting rotation...")  # Debugging statement
-
-    while not rospy.is_shutdown() and rospy.Time.now() < end_time:
-        twist_msg = Twist()
-        twist_msg.angular.z = 3.14
-        pub.publish(twist_msg)
-        print("Publishing rotation command...")  # Debugging statement
-        rate.sleep()
-
-    # Stop the robot after rotation
-    print("Stopping rotation...")  # Debugging statement
-    pub.publish(Twist())
+    move_base.goto(0.0, 0.0, 3.14)
 
 
 if __name__ == '__main__':
-    try:
-        rotate_180_degrees()
-    except rospy.ROSInterruptException:
-        print("Rotation interrupted.")  # Debugging statement
+    rospy.init_node("test_arm_poses")
+
+    move_base = MoveBaseClient()
+
+    rospy.sleep(1)
+
+    rotate_180_degrees()
+
+    rospy.spin()
