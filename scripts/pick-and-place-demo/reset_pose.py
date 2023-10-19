d97945cfa9da8af4771ef54dcaaed1cca2458f38
Kickblip <wyatt.zilker@gmail.com>
2023-10-18 22:13:37 -0600

    add head reset

--- scripts/pick-and-place-demo/reset_pose.py
+++ scripts/pick-and-place-demo/reset_pose.py
@@ -5,7 +5,29 @@ from moveit_msgs.msg import MoveItErrorCodes
 from moveit_python import MoveGroupInterface, PlanningSceneInterface
 
 
+class PointHeadClient(object):
+
+    def __init__(self):
+        self.client = actionlib.SimpleActionClient(
+            "head_controller/point_head", PointHeadAction)
+        rospy.loginfo("Waiting for head_controller...")
+        self.client.wait_for_server()
+
+    def look_at(self, x, y, z, frame, duration=1.0):
+        goal = PointHeadGoal()
+        goal.target.header.stamp = rospy.Time.now()
+        goal.target.header.frame_id = frame
+        goal.target.point.x = x
+        goal.target.point.y = y
+        goal.target.point.z = z
+        goal.min_duration = rospy.Duration(duration)
+        self.client.send_goal(goal)
+        self.client.wait_for_result()
+
+
 def move_robot():
+
+    head_action = PointHeadClient()
 
     move_group = MoveGroupInterface("arm", "base_link")
     planning_scene = PlanningSceneInterface("base_link")
@@ -27,6 +49,8 @@ def move_robot():
                          move_group.get_move_action().get_state())
     else:
         rospy.logerr("MoveIt! failure no result returned.")
+
+    head_action.look_at(0.0, 0.0, 0.0, "base_link")
 
 
 if __name__ == '__main__':
