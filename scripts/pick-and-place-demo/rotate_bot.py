a6d4562b4ffec57008d394dd26f801a5b5907b5f
Kickblip <wyatt.zilker@gmail.com>
2023-10-18 23:41:29 -0600

    forgot shebang

--- scripts/pick-and-place-demo/rotate_bot.py
+++ scripts/pick-and-place-demo/rotate_bot.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
 import rospy
 from geometry_msgs.msg import Twist
 
 
-def rotate_180_degrees():
+def continuous_rotate(duration):
     # Initialize the node
-    rospy.init_node('rotate_180_degrees', anonymous=True)
+    rospy.init_node('continuous_rotate', anonymous=True)
 
     # Create a publisher to the cmd_vel topic
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
@@ -16,25 +18,24 @@ def rotate_180_degrees():
     rotate_cmd = Twist()
 
     # Set the angular velocity to a value that will rotate the robot. Adjust this value based on your robot's capabilities
-    # For example, rotate at 1 rad/s. Adjust this value as needed
+    # For example, rotate at 1 rad/s. Adjust this value as needed.
     rotate_cmd.angular.z = 1.0
 
-    # Compute the rotation time for 180 degrees based on the given angular velocity
-    rotation_time = 3.14 / rotate_cmd.angular.z
-
-    # Publish the rotation command
-    pub.publish(rotate_cmd)
-
-    # Wait for the rotation to complete
-    rospy.sleep(rotation_time)
+    # Publish the rotation command for the desired duration
+    end_time = rospy.Time.now() + rospy.Duration(duration)
+    while rospy.Time.now() < end_time:
+        pub.publish(rotate_cmd)
+        # This will publish the command at 10Hz, adjust as needed
+        rospy.sleep(0.1)
 
     # Stop the robot by publishing a zero Twist message
-    # stop_cmd = Twist()
-    # pub.publish(stop_cmd)
+    stop_cmd = Twist()
+    pub.publish(stop_cmd)
 
 
 if __name__ == '__main__':
     try:
-        rotate_180_degrees()
+        # Rotate for 5 seconds as an example. Change the duration as needed.
+        continuous_rotate(5)
     except rospy.ROSInterruptException:
         pass
