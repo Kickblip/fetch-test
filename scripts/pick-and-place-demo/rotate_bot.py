bf5abe75756e53532da83fa3bcb63991a5876a7e
Kickblip <wyatt.zilker@gmail.com>
2023-10-18 22:43:23 -0600

    int

--- scripts/pick-and-place-demo/rotate_bot.py
+++ scripts/pick-and-place-demo/rotate_bot.py
@@ -11,16 +11,13 @@ def rotate_180_degrees():
     # Publisher for cmd_vel
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
-    # Assuming it takes 2 seconds for a full 360 degree rotation at maximum speed
-    # Adjust this time based on your robot's speed
-    rotation_duration = 1.0  # Time it takes to rotate 180 degrees
-    rate = rospy.Rate(10)  # 10Hz
+    rotation_duration = 1.0
+    rate = rospy.Rate(10)
     end_time = rospy.Time.now() + rospy.Duration(rotation_duration)
 
     while not rospy.is_shutdown() and rospy.Time.now() < end_time:
         twist_msg = Twist()
-        # Assuming pi rad/sec is the maximum speed. Adjust as necessary.
-        twist_msg.angular.z = pi
+        twist_msg.angular.z = 3.14
         pub.publish(twist_msg)
         rate.sleep()
 
